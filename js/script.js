9ada7e38605eb4e6d5893c580e521881dce07fab
Ankush Arora <aaforankusharora@gmail.com>
2024-07-09 14:39:37 +0530

    edited

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('nav ul li a');
+
+    for (const link of links) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop - 60,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.info-button');
-    
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert('More information about this project.');
-        });
-    });
-});
