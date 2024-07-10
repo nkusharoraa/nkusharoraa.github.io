c7edda8ae320be1250c2126c48c7242ff61feed7
Ankush Arora <aaforankusharora@gmail.com>
2024-07-10 16:29:19 +0530

    csd

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const modeToggle = document.getElementById('mode-toggle');
     const modeIcon = document.getElementById('mode-icon');
+    const shareBtn = document.getElementById('share-btn');
     
     modeToggle.addEventListener('click', function() {
         // Add a delay before toggling dark mode
@@ -13,5 +14,28 @@ document.addEventListener('DOMContentLoaded', function() {
                 modeIcon.src = 'toggle-dark-icon.svg';
             }
         }, 800); // Adjust delay in milliseconds (800ms in this example for slower effect)
+    });
+    
+    shareBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        const linkToCopy = 'https://nkusharoraa.github.io/';
+        
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(linkToCopy).then(function() {
+                alert('Link copied to clipboard: ' + linkToCopy);
+            }).catch(function(err) {
+                console.error('Failed to copy text: ', err);
+            });
+        } else {
+            // Fallback for older browsers
+            const dummy = document.createElement('input');
+            document.body.appendChild(dummy);
+            dummy.value = linkToCopy;
+            dummy.select();
+            document.execCommand('copy');
+            document.body.removeChild(dummy);
+            alert('Link copied to clipboard: ' + linkToCopy);
+        }
     });
 });
